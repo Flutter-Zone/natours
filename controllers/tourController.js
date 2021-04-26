6a447cf3ce3e2bfc4715cbe3c169323c58876e3f
Flutter Zone <letsflatter@gmail.com>
2021-04-26 16:32:28 +0000

    implemented simple CRUD with mongoose

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,61 +1,102 @@
+const { response } = require('../app');
 const Tour = require('./../models/tourModel');
 
 
-exports.checkBody = (req, res, next) => {
-    const body = req.body;
-    if(!body.name || !body.price){
-        // 400 means - bad request
-        return res.status(400).json({
-            status: "failure",
-            message: "Missing name or price"
+
+exports.getAllTours = async (req,res) => {
+
+    try{
+
+        const tours = await Tour.find();
+
+        res.status(200).json({
+            status: 'success',
+            requestedaAt: req.requestTime,
+            results: tours.length,
+            data: {
+                tours
+            }
+        });
+    }catch(err){
+        res.status(404).json({
+            status: 'fail',
+            message: 'Invalid request sent'
         });
     }
-    next();
+
 }
 
-exports.getAllTours = (req,res) => {
-    res.status(200).json({
-        status: 'success',
-        requestedaAt: req.requestTime,
-        // results: tours.length,
-        // data: {
-        //     tours
-        // }
-    });
+exports.getTour = async (req,res) => {
+    try{
+        // const id = req.params.id * 1;
+        const tour = await Tour.findById(req.params.id);
+        // Tour.findOne({ id: req.params.id })
+        res.status(200).json({
+            status: 'success', 
+            data: {
+                tour
+            }
+        });
+    }catch(err){
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        });
+    }
 }
 
-exports.getTour = (req,res) => {
-    // const id = req.params.id * 1;
-    // const tour = tours.find(el => el.id === id);
-    res.status(200).json({
-        status: 'success', 
-        // data: {
-        //     tour
-        // }
-    });
+exports.createTour = async (req, res) => {
+    // const newTour = new Tour();
+    // newTour.save();
+
+    try{
+        const document = await Tour.create(req.body);
+    
+        res.status(201).json({
+            status: 'Success',
+            data: {
+                tour: document
+            }
+        });
+    }catch(err){
+        res.status(404).json({
+            status: 'fail',
+            message: 'Invalid data sent'
+        });
+    }
 }
 
-exports.createTour = (req, res) => {
-    res.status(201).json({
-        status: 'Success',
-        data: {
-            tour
-        }
-    });
+exports.updateTour = async (req, res) => {
+    try{
+        const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+        res.status(200).json({
+            status: 'success',
+            data: {
+                tour
+            }
+        });
+    }catch(err){
+        res.status(404).json({
+            status: 'fail',
+            message: 'Invalid data sent'
+        });
+    }
 }
 
-exports.updateTour = (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        // data: {
-        //     tour : '<Update tour here ...>'
-        // }
-    });
-}
-
-exports.deleteTour = (req, res) => {
-    res.status(204).json({
-        status: 'sucess',
-        data: null
-    });
+exports.deleteTour = async (req, res) => {
+    try{
+        await Tour.findByIdAndDelete(req.params.id);
+        res.status(204).json({
+            status: 'success',
+            data: null
+        });
+    }catch(err){
+        res.status(404).json({
+            status: 'fail',
+            message: 'Invalid data sent'
+        });
+    }
 }
