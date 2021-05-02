edb077a7cd84a53f095ca02c4fd5d2ff5e99890f
Flutter Zone <letsflatter@gmail.com>
2021-05-02 20:35:44 +0000

    implemented data sanitization and prevented parameter pollution

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize  = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -11,26 +15,57 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1) MIDDLEWARES
+
+// its better to place the helmet middleware before any other middleware
+app.use(helmet());
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// limit request from same IP
 const limiter = rateLimit({
   // the number should be greate if you are building an api that will handle lots of requests
-  max: 3,
+  max: 100,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, plese try again in an hour!'
 });
-
 app.use('/api', limiter);
-app.use(express.json());
+
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb'}));
+
+// Data sanitization against NoSQL from query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration', 
+      'ratingsQuantity', 
+      'ratingsAverage', 
+      'maxGroupSize', 
+      'difficulty',
+      'price'
+    ]
+  })
+);
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
+// ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
@@ -38,6 +73,7 @@ app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
+// global error middleware
 app.use(globalErrorHandler);
 
 module.exports = app;
