af1cf9619e9fc19197e7a8cfb2a7e04b517d008d
Flutter Zone <letsflatter@gmail.com>
2021-05-04 21:12:16 +0000

    added the review cards for the tour detail

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -5,16 +5,16 @@ const router = express.Router();
 
 
 // VIEW ROUTES
-router.get('/', (req, res) => {
-    res.status(200).render('base', {
-        tour: 'The Forest Hiker',
-        user: 'Blankson'
-    });
-});
+// router.get('/', (req, res) => {
+//     res.status(200).render('base', {
+//         tour: 'The Forest Hiker',
+//         user: 'Blankson'
+//     });
+// });
 
 
-router.get('/overview', viewController.getOverview);
-router.get('/tour', viewController.getTour);
+router.get('/', viewController.getOverview);
+router.get('/tours/:slug', viewController.getTour);
   
 
 module.exports = router;
