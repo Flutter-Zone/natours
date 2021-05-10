303ceee5bbd78f3c6ea2818fd4aee308f368e6de
Flutter Zone <letsflatter@gmail.com>
2021-05-10 15:18:21 +0000

    updating user password implemented

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,6 @@
 import '@babel/polyfill'; // to polyfill some of the features of javascript
 import { login, logout } from './login';
-import { updateData } from './updateSettings';
+import { updateSettings } from './updateSettings';
 import { displayMap } from './mapbox';
 
 
@@ -9,7 +9,7 @@ const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
-
+const userSettingsForm = document.querySelector('.form-user-settings');
 
 // delegation
 if(mapBox){
@@ -31,12 +31,33 @@ if(logOutBtn) logOutBtn.addEventListener('click', logout);
 
 if(userDataForm){
   userDataForm.addEventListener('submit', e => {
-    console.log('Was called');
     e.preventDefault();
 
     const email = document.getElementById('email').value;
     const name = document.getElementById('name').value;
 
-    updateData(name, email);
+    updateSettings({name, email}, 'data');
   })
 }
+
+
+if(userSettingsForm){
+  userSettingsForm.addEventListener('submit', async e => {
+    e.preventDefault();
+
+    document.querySelector('.btn--save-password').textContent = 'Updating...';
+
+    const currentPassword = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('password-confirm').value;
+
+    await updateSettings({currentPassword, password, confirmPassword}, 'password');
+
+    document.querySelector('.btn--save-password').textContent = 'Save Password';
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
+     
+  });
+}
+
